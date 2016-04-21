ec3309bc27c05001f8020c06c6cb5576f1fa53db
juhlmadsen1982@gmail.com <juhlmadsen1982@gmail.com>
2016-04-21 07:45:20 +0200

    Initial commit

---
+++ questions2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by steffen on 21-04-2016.
+ */
+
+
+//Explain polling and long-polling strategies, their pros and cons.
+//Polling strategy
+//A client will make a request to the server, and the server will send a response.  Its just like normal http traffic
---
+++ questions3.js
@@ -0,0 +1,8 @@
+/**
+ * Created by steffen on 21-04-2016.
+ */
+
+
+// Long-polling strategi
+//A client will sent a request to a server, the server will wait until there are something to response with, and when send that to the client…
+//the client will immediately send a new request to the server… and so on.
